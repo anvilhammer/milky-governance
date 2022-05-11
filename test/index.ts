ac926361cf15f7f5ce715d8e4fb7987856bd6515
MeccaMilkMan <mecca.milk.man@protonmail.com>
2022-05-11 20:25:10 +0300

    initial testing

--- test/index.ts
+++ test/index.ts
@@ -1,28 +1,87 @@
 import { expect } from 'chai';
 import { ethers } from 'hardhat';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import {
   MilkyGovernorDelegate__factory,
+  MilkyGovernorDelegate,
   MilkyGovernorDelegator__factory,
-  Creamy__factory,
+  CreamyToken__factory,
+  CreamyToken,
+  ERC20Mock__factory,
 } from '../dist/types'
+import {
+  TEN_18,
+  ZERO_ADDRESS,
+} from './utils'
+
+const VOTING_PERIOD = ethers.BigNumber.from(151200)
+const VOTING_DELAY = ethers.BigNumber.from(1)
+const PROPOSAL_THRESHOLD = TEN_18.mul(50000)
 
 describe('MilkyGovernor State', () => {
-  beforeEach(async () => {
+  let deployer: SignerWithAddress,
+      alice: SignerWithAddress,
+      bob: SignerWithAddress,
+      carol: SignerWithAddress
 
+  let milky, creamy: CreamyToken, gov: MilkyGovernorDelegate, govDelegate
+
+  before(async () => {
+    [deployer, alice, bob, carol] = await ethers.getSigners()
+
+    const Milky = new ERC20Mock__factory(deployer)
+    milky = await Milky.deploy('MilkySwap Token', 'MILKY', TEN_18.mul(1_000_000))
+    await milky.deployed()
+
+    const Creamy = new CreamyToken__factory(deployer)
+    creamy = await Creamy.deploy(milky.address, 'CREAMY', 'CREAMY', '1')
+    await creamy.deployed()
+
+    const GovDelegate = new MilkyGovernorDelegate__factory(deployer)
+    govDelegate = await GovDelegate.deploy()
+    await govDelegate.deployed()
+
+    const GovInstance = new MilkyGovernorDelegator__factory(deployer)
+    const govInstance = await GovInstance.deploy(
+      ZERO_ADDRESS,
+      creamy.address,
+      deployer.address,
+      govDelegate.address,
+      VOTING_PERIOD,
+      VOTING_DELAY,
+      PROPOSAL_THRESHOLD,
+    )
+    await govInstance.deployed()
+
+    gov = govDelegate.attach(govInstance.address) as MilkyGovernorDelegate
+    await gov._initiate(ZERO_ADDRESS)
   })
 
-  it('Should return the new greeting once it\'s changed', async () => {
-    // const Greeter = await ethers.getContractFactory('Greeter');
-    // const greeter = await Greeter.deploy('Hello, world!');
-    // await greeter.deployed();
+  describe('initialize', () => {
+    it('Set correct initial values', async () => {
+      expect(await gov.admin()).eq(deployer.address)
+      expect(await gov.initialProposalId()).eq(1)
+      expect(await gov.creamy()).eq(creamy.address)
+      expect(await gov.votingDelay()).eq(VOTING_DELAY)
+      expect(await gov.votingPeriod()).eq(VOTING_PERIOD)
+      expect(await gov.timelock()).eq(ZERO_ADDRESS)
+      expect(await gov.proposalThreshold()).eq(PROPOSAL_THRESHOLD)
+    })
+  })
 
-    // expect(await greeter.greet()).to.equal('Hello, world!');
+  describe('propose', () => {
+    it('asdf')
+  })
 
-    // const setGreetingTx = await greeter.setGreeting('Hola, mundo!');
+  describe('castVote', () => {
+    it('asdf')
+  })
 
-    // // wait until the transaction is mined
-    // await setGreetingTx.wait();
+  describe('whitelist', () => {
+    it('asdf')
+  })
 
-    // expect(await greeter.greet()).to.equal('Hola, mundo!');
-  });
-});
+  describe('upgrade', () => {
+    it('asdf')
+  })
+})
---
+++ test/utils.ts
@@ -0,0 +1,4 @@
+import { BigNumber } from 'ethers'
+
+export const TEN_18 = BigNumber.from('1000000000000000000')
+export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
